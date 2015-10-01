270250386da3a3528aaaaa5addeafc98f1d70447
Rich <piemonkey@gmail.com>
2015-10-01 10:51:38 +0100

    Change gifts controller to get data from mongodb

--- controllers/gifts.js
+++ controllers/gifts.js
@@ -1,9 +1,17 @@
+var mongoClient = require('mongodb').MongoClient
+;
+
 var giftsController = {
   getHome: function(req, res) {
-    res.render('gifts', { title: 'Gifts', gift: [
-      {name: 'Free thing', description: 'This thing is free'},
-      {name: 'Free help', description: 'I\'ll help you all for free'}
-    ]});
+    mongoClient.connect('mongodb://localhost:27017/gift-economy', function(err, db) {
+      // TODO Error management
+      var collection = db.collection('gifts');
+      collection.find({}).toArray(function(err, gifts) {
+        console.log(gifts);
+        res.render('gifts', { title: 'Gifts', gift: gifts});
+        db.close();
+      })
+    });
   }
 }
 
--- test/controllers/gifts.js
+++ test/controllers/gifts.js
@@ -1,20 +1,41 @@
 var assert = require('assert')
+  , rewire = require('rewire')
 ;
 
-var controller;
+var controller,
+  mongoClient;
 
 describe('Gifts controller', function() {
 
   beforeEach(function() {
-    controller = require('../../controllers/gifts');
+    controller = rewire('../../controllers/gifts');
+    mongoClient = {};
+    controller.__set__('mongoClient', mongoClient);
   });
 
-  it('renders an example homepage', function(done) {
+  it('renders a homepage with mongo data', function(done) {
+    giftData = {};
+    var db = {
+      collection: function(name) {
+        assert.equal(name, 'gifts');
+        return {
+          find: function() {
+            return {
+              toArray: function(callback) {
+                callback({}, giftData)
+              }
+            }
+          }
+        };
+      },
+      close: function() {}
+    };
+    mongoClient.connect = function(url, callback) {callback({}, db)};
     controller.getHome({}, {
       render: function(template, data) {
         assert.strictEqual(template, 'gifts', 'Template name');
-        assert.notEqual(data, undefined, 'Template needs data to be passed to it');
-        assert.notDeepEqual(data, {}, 'Cannot pass empty data to template');
+        assert.equal(data.title, 'Gifts', 'Template must have title set correctly');
+        assert.equal(data.gift, giftData, 'Must pass mongo data to the template');
         done();
       }
     });
